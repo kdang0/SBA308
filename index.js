51f54428636e1af289a0bffc964332c12cbaf168
Kevin Dang <kevin.kd.dang@gmail.com>
2024-08-18 00:48:15 -0400

    created a helper function to calculate learner's score for assignment

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ const LearnerSubmissions = [
       score: 140,
     },
   },
+  //testing
+  {
+    learner_id: 150,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-06",
+      score: 130,
+    },
+  },
 ];
 
 function getLearnerData(course, ag, submissions) {
@@ -97,74 +106,119 @@ function getLearnerData(course, ag, submissions) {
 
   //checks to see if specific assignment is due
   function isDue(aDueDate) {
-    let currentDate = new Date();
-    let assignmentDate = new Date(aDueDate);
-    if (currentDate < assignmentDate) {
-      return false;
-    } else {
-      return true;
+    try {
+      let currentDate = new Date();
+      let assignmentDate = new Date(aDueDate);
+      if (currentDate < assignmentDate) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (error) {
+      return error;
     }
   }
 
   //calculates learner's score for that specific assignment
   function calculateScore(score, sDate, aDate, pointsPossible) {
-    let subDate = new Date(sDate);
-    let assiDate = new Date(aDate);
-    subDate > assiDate ? (score -= pointsPossible*0.10) : score;
-    return score;
+    try {
+      let subDate = new Date(sDate);
+      let assiDate = new Date(aDate);
+      if (isNaN(score)) throw "ERROR: score is not a valid number, omitting...";
+      subDate > assiDate ? (score -= pointsPossible * 0.1) : score;
+      return score;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  //updates scores of specific learner object based on their assignment submission
+  function updateLearnerScores(submission, assignments) {
+    for (let assignment of assignments) {
+      if (submission.assignment_id === assignment.id) {
+        if (isDue(assignment.due_at)) {
+          console.log("==================\n     IS DUE\n==================");
+          try {
+            if (isNaN(assignment.points_possible))
+              throw "ERROR: Points possible is not a valid number, omitting...";
+            if (assignment.points_possible <= 0)
+              throw "ERROR: Points possible is less than or equal to zero, omitting...";
+            if (
+              isNaN(
+                calculateScore(
+                  submission.submission.score,
+                  submission.submission.submitted_at,
+                  assignment.due_at,
+                  assignment.points_possible
+                )
+              )
+            )
+              throw `Having issues calculating learner's score; ${calculateScore(
+                submission.submission.score,
+                submission.submission.submitted_at,
+                assignment.due_at,
+                assignment.points_possible
+              )}`;
+            learner[assignment.id] =
+              calculateScore(
+                submission.submission.score,
+                submission.submission.submitted_at,
+                assignment.due_at,
+                assignment.points_possible
+              ) / assignment.points_possible;
+            totalScore += assignment.points_possible;
+            currentScore += calculateScore(
+              submission.submission.score,
+              submission.submission.submitted_at,
+              assignment.due_at,
+              assignment.points_possible
+            );
+          } catch (error) {
+            return error;
+          }
+        } else {
+          console.log("==================\n   NOT DUE YET\n==================");
+          break;
+        }
+      }
+    }
+    return `Finished update: ${JSON.stringify(learner)}`;
   }
 
   const result = [];
   let totalScore = 0;
   let currentScore = 0;
   let currentLearner = submissions[0].learner_id;
-  let learner = {};
+  let learner = {
+    id: currentLearner,
+  };
+  learner.id = currentLearner;
   if (ag.course_id === course.id) {
     //parse through assignment group to see if its valid
     for (let i = 0; i < submissions.length; i++) {
-      if (
-        submissions[i].learner_id !== currentLearner
-      ) {
-        learner.id = currentLearner;
+      if (submissions[i].learner_id !== currentLearner) {
+        //calculates average score for all assignments for that specific learner
         learner.avg = currentScore / totalScore;
         result.push(learner);
+        //Set learner to an empty object
         learner = {};
         currentLearner = submissions[i].learner_id;
+        learner.id = currentLearner;
         totalScore = 0;
         currentScore = 0;
       }
-      //checks to see if assignment from the submission is due yet
-      for (let assignment of ag.assignments) {
-        if (submissions[i].assignment_id === assignment.id) {
-          if (isDue(assignment.due_at)) {
-            console.log("IS DUE");
-            learner[assignment.id] =
-              calculateScore(
-                submissions[i].submission.score,
-                submissions[i].submission.submitted_at,
-                assignment.due_at,
-                assignment.points_possible
-              ) / assignment.points_possible;
-            totalScore += assignment.points_possible;
-            currentScore += calculateScore(
-              submissions[i].submission.score,
-              submissions[i].submission.submitted_at,
-              assignment.due_at,
-              assignment.points_possible
-            );
-          } else {
-            console.log("NOT DUE YET");
-          }
-        }
-      }
+      /**
+       * Updates a learner's score percentage for that specific assignment submitted
+       */
+      const statusLog = updateLearnerScores(submissions[i], ag.assignments);
+      //print out the result after parsing through that particular assignment and submission
+      console.log(statusLog);
     }
     learner.id = currentLearner;
     learner.avg = currentScore / totalScore;
     result.push(learner);
   } else {
-    console.error(
-      "The course id of the assignment group is not the same as the course provided."
-    );
+    return "The course id of the assignment group is not the same as the course provided.";
   }
   return result;
 }
